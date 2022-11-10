d088a4c0fd92cb9b8135954d1e3d614c4f9df962
zonemeen <miqilin18@gmail.com>
2022-11-10 13:35:31 +0800

    feat: 不缓存错误时的响应

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import crypto from 'crypto'
 import ejs from 'ejs'
 import axios from 'axios'
+import { renderError } from '../src/utils.js'
 
 const readTemplateFile = () => readFileSync(resolve('svg.ejs'), 'utf-8')
 
@@ -25,63 +26,68 @@ export default async (request, response) => {
     title = 'Recently Played',
     width = 280,
     size = '800',
-    show_percent = '0'
+    show_percent = '0',
   } = request.query
 
-  const { data } = await axios.post(
-    'https://music.163.com/weapi/v1/play/record?csrf_token=',
-    aesRsaEncrypt(
-      JSON.stringify({
-        uid: id,
-        type,
-      })
-    ),
-    {
-      headers: {
-        Accept: '*/*',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
-        Connection: 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        Host: 'music.163.com',
-        Referer: 'https://music.163.com/',
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
-        Cookie:
-          'os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; ',
-      },
-    }
-  )
-
-  const songs = data[Number(type) === 1 ? 'weekData' : 'allData'].slice(0, Number(number))
-
-  const buffers = await Promise.all(
-    songs.map(({ song }) =>
-      axios.get(`${song.al.picUrl}${size !== '800' ? `?param=${size}x${size}` : ''}`, {
-        responseType: 'arraybuffer',
-      })
-    )
-  )
-
-  const covers = buffers.map((buffer) => {
-    const buffer64 = Buffer.from(buffer.data, 'binary').toString('base64')
-    return `data:image/jpg;base64,` + buffer64
-  })
-
-  const templateParams = {
-    recentPlayed: songs.map(({ song, score }, i) => {
-      return {
-        name: song.name,
-        artist: song.ar.map(({ name }) => name).join('/'),
-        cover: covers[i],
-        url: `https://music.163.com/#/song?id=${song.id}`,
-        percent: show_percent === '1' ? score / 100 : 0
+  try {
+    const { data } = await axios.post(
+      'https://music.163.com/weapi/v1/play/record?csrf_token=',
+      aesRsaEncrypt(
+        JSON.stringify({
+          uid: id,
+          type,
+        })
+      ),
+      {
+        headers: {
+          Accept: '*/*',
+          'Accept-Encoding': 'gzip,deflate,sdch',
+          'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
+          Connection: 'keep-alive',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          Host: 'music.163.com',
+          Referer: 'https://music.163.com/',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+          Cookie:
+            'os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; ',
+        },
       }
-    }),
-    themeConfig: { title, width: Number(width) },
+    )
+
+    const songs = data[Number(type) === 1 ? 'weekData' : 'allData'].slice(0, Number(number))
+
+    const buffers = await Promise.all(
+      songs.map(({ song }) =>
+        axios.get(`${song.al.picUrl}${size !== '800' ? `?param=${size}x${size}` : ''}`, {
+          responseType: 'arraybuffer',
+        })
+      )
+    )
+
+    const covers = buffers.map((buffer) => {
+      const buffer64 = Buffer.from(buffer.data, 'binary').toString('base64')
+      return `data:image/jpg;base64,` + buffer64
+    })
+
+    const templateParams = {
+      recentPlayed: songs.map(({ song, score }, i) => {
+        return {
+          name: song.name,
+          artist: song.ar.map(({ name }) => name).join('/'),
+          cover: covers[i],
+          url: `https://music.163.com/#/song?id=${song.id}`,
+          percent: show_percent === '1' ? score / 100 : 0,
+        }
+      }),
+      themeConfig: { title, width: Number(width) },
+    }
+    response.setHeader('Cache-Control', 'public, max-age=14400')
+    response.setHeader('content-type', 'image/svg+xml')
+    response.statusCode = 200
+    response.send(ejs.render(readTemplateFile(), templateParams))
+  } catch (err) {
+    response.setHeader('Cache-Control', `no-cache, no-store, must-revalidate`)
+    return response.send(renderError(err.message, err.secondaryMessage))
   }
-  response.setHeader('Cache-Control', 'public, max-age=14400')
-  response.setHeader('content-type', 'image/svg+xml')
-  response.statusCode = 200
-  response.send(ejs.render(readTemplateFile(), templateParams))
 }
---
+++ src/utils.js
@@ -0,0 +1,28 @@
+const encodeHTML = (str) => {
+  return str
+    .replace(/[\u00A0-\u9999<>&](?!#)/gim, (i) => {
+      return '&#' + i.charCodeAt(0) + ';'
+    })
+    .replace(/\u0008/gim, '')
+}
+
+const renderError = (message, secondaryMessage = '') => {
+  return `
+    <svg width="724" height="120" viewBox="0 0 724 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <style>
+    .text a { font: 600 16px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2F80ED; text-decoration: underline }
+    .text { font: 600 16px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2F80ED }
+    .small { font: 600 12px 'Segoe UI', Ubuntu, Sans-Serif; fill: #252525 }
+    .gray { fill: #858585 }
+    </style>
+    <rect x="0.5" y="0.5" width="723" height="99%" rx="4.5" fill="#FFFEFE" stroke="#E4E2E2"/>
+    <text x="25" y="45" class="text">Something went wrong! file an issue at <a id="netease" target="_blank" href="https://github.com/zonemeen/netease-recent-profile">https://github.com/zonemeen/netease-recent-profile</a></text>
+    <text data-testid="message" x="25" y="55" class="text small">
+      <tspan x="25" dy="18">${encodeHTML(message)}</tspan>
+      <tspan x="25" dy="18" class="gray">${secondaryMessage}</tspan> 
+    </text>
+    </svg>
+  `
+}
+
+export { renderError }
